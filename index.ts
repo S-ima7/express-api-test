10525b850f350cdb4b4821fe08033b5ac2a619cd
musicangora <musicangora@gmail.com>
2025-02-12 17:04:29 +0900

    feat: make conflict

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 8080
 
-app.get("/", (req, res) => {
-  res.send("今日も大好き！");
-});
+// コンフリクトを起こす
+
+app.get('/', (req, res) => {
+  res.send('今日も大好き！')
+})
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
