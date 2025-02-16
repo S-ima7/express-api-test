7ddb711f170100e3eb804011a24fe7f8c48816f5
S-ima7 <knotonlybutalso@gmail.com>
2025-02-16 13:46:49 +0900

    chore: get削除

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,7 @@ app.use(express.json())
 // public ディレクトリを静的ファイルとして提供
 app.use(express.static(path.join(__dirname, '../public')))
 
-// "/" へのアクセスで index.html を返す
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'))
-})
+
 
 app.listen(port, () => {
   console.log(`server start at http://localhost:${port}`)
