3eef6998faa338ece50f9fe6f54924242c9ac78f
S-ima7 <knotonlybutalso@gmail.com>
2025-02-16 17:34:52 +0900

    chore: API処理の単純化(重複削除)

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080
 app.use(express.json())
 
 // public ディレクトリを静的ファイルとして提供
-app.use(express.static(path.join(__dirname, '../public')))
+app.use("/", express.static(path.join(__dirname, '../public')))
 
 
 
