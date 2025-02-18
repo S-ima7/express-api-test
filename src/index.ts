cafbe25306adefbaf7c40d4d68a991c0756fb378
S-ima7 <knotonlybutalso@gmail.com>
2025-02-18 15:51:28 +0900

    chore: "Prettierの導入に伴うファイルの追加"

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import path from 'path'
 const app = express()
 const port = process.env.PORT || 8080
 
-
 // public ディレクトリを静的ファイルとして提供
-app.use("/", express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')))
 app.listen(port, () => {
   console.log(`server start at http://localhost:${port}`)
 })
